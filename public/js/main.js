700c72310dbb12e3e82a1c7c582b9bbf30633122
Delsart Ivan <ivandelsart@gmail.com>
2023-03-07 12:54:22 -0300

    modificacion colores chat

--- public/js/main.js
+++ public/js/main.js
@@ -82,7 +82,7 @@ function outputUsers(users) {
 
 //Prompt the user before leave chat room
 document.getElementById('leave-btn').addEventListener('click', () => {
-  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+  const leaveRoom = confirm('Estas seguro que quieres abandonar el chat?');
   if (leaveRoom) {
     window.location = '../index.html';
   } else {
--- server.js
+++ server.js
@@ -39,7 +39,7 @@ io.on("connection", (socket) => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
+    socket.emit("message", formatMessage(botName, "Bienvenido a QuePintaParana-Chat!"));
 
     // Broadcast when a user connects
     socket.broadcast
